bdc62cd8ddad6d1bdecd243e4c1db5c63020ff75
vvmaas <vhvermaas@gmail.com>
2022-06-10 21:55:20 -0300

    falta resolver o bug do js e o whatsapp

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-function inicial() {
-    let escolhido = document.querySelector(".card")
-    escolhido.classList.toggle("selecionado")
+let escolhidoSobre;
+let escolhidoPrato;
+let escolhidoBebe;
+
+function selecionarPrato(item) {
+    escolhidoPrato = document.querySelector(".prato .selecionado");
+    if (escolhidoPrato !== null) {
+        escolhidoPrato.classList.remove("selecionado");
+    }
+    item.classList.add("selecionado");
+    liberaFechar();
 }
-inicial()
+
+function selecionarBebe(item) {
+    escolhidoBebe = document.querySelector(".bebida .selecionado");
+    if (escolhidoBebe !== null) {
+        escolhidoBebe.classList.remove("selecionado");
+    }
+    item.classList.add("selecionado");
+    liberaFechar();
+}
+
+function selecionarSobre(item) {
+    escolhidoSobre = document.querySelector(".sobremesa .selecionado")
+    if (escolhidoSobre !== null) {
+        escolhidoSobre.classList.remove("selecionado");
+    }
+    item.classList.add("selecionado");
+    liberaFechar();
+}
+
+function liberaFechar() {
+    if (escolhidoPrato && escolhidoBebe && escolhidoSobre){
+        let bselecionar = document.querySelector(".botao-selecionar");
+        let bfechar = document.querySelector(".botao-fechar");
+        bfechar.classList.remove("escondido");
+        bselecionar.classList.add("escondido");
+    }
+}
